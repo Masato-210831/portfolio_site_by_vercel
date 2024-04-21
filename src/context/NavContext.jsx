d7190b04fc47e144ced7549e3f8699416e652e8e
masato-210831 <b86500b@gmail.com>
2024-04-21 11:44:12 +0900

    deploy for vecel

---
+++ src/context/NavContext.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext } from "react";
+
+const navItemsList = ["Home", "About", "Works", "Products"];
+const NavItemsContext = createContext()
+
+// vite.configのbase
+const appBase = './'
+const APPContext = createContext()
+
+// App.jsxで囲む
+export const NavItemsProvider = ({children}) => {
+  return (
+    <NavItemsContext.Provider value={navItemsList}>
+      <APPContext.Provider value={appBase}>
+        {children}
+      </APPContext.Provider>
+    </NavItemsContext.Provider>
+  )
+}
+
+// 他のコンポーネントで使用する
+export const useNavItems = () => useContext(NavItemsContext)
+
+// vite.configのbase -> 画像のリンクに使用
+export const useAppBase = () => useContext(APPContext)
